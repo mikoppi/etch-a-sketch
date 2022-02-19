907db0b9da45c4641dc45c62df2034b52af2f97d
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-02-19 18:48:45 +0200

    Create raw version

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-//DOM script to create 16x16 div grid
 const container=document.querySelector('.container');
-for(let i=1;i<=(16*16);i++) {
-    let grid=document.createElement('div');
-    grid.className=i;
-    container.appendChild(grid);   
-}
 
-//Add event listener "hover" for each div in the grid
-const grids=container.querySelectorAll('div');
-grids.forEach((div) => {
-    div.addEventListener('mouseenter',colorGrid)
+const buttonReset=document.querySelector('#reset');
+    buttonReset.addEventListener('click',clearGridAndAskLenght)
 
-})
+/*const buttonStart=document.querySelector('#start');
+    buttonStart.addEventListener('click',() => {
+        let gridAmount=prompt("How many squares per side?");
+        createGrid(gridAmount);
+    })*/
 
 //function that colors grid when "mouseenter" listener activates
 function colorGrid(e) {
-    e.style.color='blue';
-    console.log("colored");
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = "#" + randomColor;
+}
+
+//function that clears the grid
+function clearGridAndAskLenght (e) {
+    const grids=container.querySelectorAll('div');
+    grids.forEach((div) => {
+        div.setAttribute('style','background: white');
+    })
+    let gridAmount=prompt("How many squares per side?");
+    createGrid(gridAmount);
+}
+
+//function that asks for the number of squares per side
+function createGrid(gridAmount) {
+    for(let i=1;i<=(gridAmount*gridAmount);i++) {
+        let grid=document.createElement('div');
+        grid.className=i;
+        let sideLenght=640/gridAmount;
+        console.log(sideLenght);
+        grid.style.width=sideLenght+'px';
+        grid.style.height=sideLenght+'px';
+        container.setAttribute('style','grid-template-columns: repeat(' + gridAmount + ', 1fr)');
+        container.appendChild(grid);
+    }
+    enableDraw()
+}
+
+//this function enables drawing
+function enableDraw() {
+    const grids=container.querySelectorAll('div');
+    grids.forEach((div) => {
+        div.addEventListener('mouseenter',colorGrid)    
+        })  
 }
